15a08210cdcab1e5a51696f487776536eedadd2f
Charmaine Chia <llamalitter@gmail.com>
2025-03-04 07:38:23 +0800

    adds main() function

--- get_binding_stability_agg_features.py
+++ get_binding_stability_agg_features.py
@@ -2,15 +2,18 @@ from feature_extraction.binding import get_yasara_binding_features
 from feature_extraction.stability import  get_yasara_foldx_stability_features
 from feature_extraction.aggregation import  get_aggregation_features
 
-features_to_extract = ['binding', 'stability', 'aggregation']
-combine_feature_into_ml_dataset = False
+def main():
+    features_to_extract = ['binding', 'stability', 'aggregation']
+    combine_feature_into_ml_dataset = False
 
-if 'binding' in features_to_extract:
-    get_yasara_binding_features()
+    if 'binding' in features_to_extract:
+        get_yasara_binding_features()
 
-if 'stability' in features_to_extract:
-    get_yasara_foldx_stability_features()
+    if 'stability' in features_to_extract:
+        get_yasara_foldx_stability_features()
 
-if 'aggregation' in features_to_extract:
-    get_aggregation_features()
+    if 'aggregation' in features_to_extract:
+        get_aggregation_features()
 
+if __name__ == "__main__":
+    main()
--- get_feature_importance.py
+++ get_feature_importance.py
@@ -4,19 +4,23 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)
 from ml_prediction.model_variables import featuresets
 
-# Load saved AutoGluon model
-model_name = 'WeightedEnsemble_L3'
-model_dir = './data/ml_prediction/trained_models/GOh1052/'
-y_feature = 'CategoryV3'
-x_features = featuresets['mut']
+def main():
+    # Load saved AutoGluon model
+    model_name = 'WeightedEnsemble_L3'
+    model_dir = './data/ml_prediction/trained_models/GOh1052/'
+    y_feature = 'CategoryV3'
+    x_features = featuresets['mut']
 
-# Load new data for prediction
-data = pd.read_csv('./data/ml_prediction/Input/GOh1052.csv')[x_features+[y_feature]]
+    # Load new data for prediction
+    data = pd.read_csv('./data/ml_prediction/Input/GOh1052.csv')[x_features+[y_feature]]
 
-# get predictor
-predictor = ag.TabularPredictor.load(model_dir)
+    # get predictor
+    predictor = ag.TabularPredictor.load(model_dir)
 
-# get feature importance of internally transformed features for the best model
-feature_importance = predictor.feature_importance(data, model=model_name, feature_stage='transformed')
-feature_importance.to_csv(f'./data/ml_prediction/Output/GOh1052_feature_importance_{model}.csv')
-print(feature_importance)
+    # get feature importance of internally transformed features for the best model
+    feature_importance = predictor.feature_importance(data, model=model_name, feature_stage='transformed')
+    feature_importance.to_csv(f'./data/ml_prediction/Output/GOh1052_feature_importance_{model}.csv')
+    print(feature_importance)
+
+if __name__ == "__main__":
+    main()
